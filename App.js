f57a44189a6ac5b965aba622af2613b0eadad146
pekkao <pekka.ojala@oamk.fi>
2023-02-16 06:56:26 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+const URL = "https://api.jokes.one/jod";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(()=> {
+    fetch(URL)
+      .then(response => response.json())
+      .then ((json) => {
+        console.log(json);
+        const joke = json.contents.jokes[0].joke;
+        setTitle(joke.title);
+        setText(joke.text);
+        setError(null);
+        setIsLoading(false);
+      },(error) => {
+        setError("Error retrieving joke!");
+        setIsLoading(false);
+        console.log(error);
+      })
+  },[])
+
+
+  if (isLoading) {
+    return <View style={styles.container}><ActivityIndicator size="large"/></View>
+  } else if(error) {
+    return <View style={styles.container}><Text>{error}</Text></View>
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Joke of the day</Text>
+        <Text style={styles.title}>{title}</Text>
+        <Text>{text}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +49,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 20,
+  },  
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 10,
+  }
 });
